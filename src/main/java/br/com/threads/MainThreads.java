0f553c8311490cd7bb6fcbcd55858dfe67db79a6
Santos, Alfredo <Alfredo.Santos@br.experian.com>
2018-02-16 23:18:48 -0200

    Updating language and use lombok to log trace

--- src/main/java/br/com/threads/MainThreads.java
+++ src/main/java/br/com/threads/MainThreads.java
@@ -2,23 +2,22 @@ package br.com.threads;
 
 public class MainThreads {
 
-	public static void main(String[] args) {
-		Runnable r1 = new Cavalo("Preto");
-		Runnable r2 = new Cavalo("Branco");
-		Runnable r3 = new Cavalo("Malhado");
-		Runnable r4 = new Cavalo("Marron");
+  public static void main(String[] args) {
+    Runnable r1 = new Horse("Black");
+    Runnable r2 = new Horse("White");
+    Runnable r3 = new Horse("Knightly");
+    Runnable r4 = new Horse("Brown");
 
-		Thread t1 = new Thread(r1);
-		Thread t2 = new Thread(r2);
-		Thread t3 = new Thread(r3);
-		Thread t4 = new Thread(r4);
+    Thread t1 = new Thread(r1);
+    Thread t2 = new Thread(r2);
+    Thread t3 = new Thread(r3);
+    Thread t4 = new Thread(r4);
 
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
-		
+    t1.start();
+    t2.start();
+    t3.start();
+    t4.start();
 
 
-	}
+  }
 }
